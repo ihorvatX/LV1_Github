30232199e328e274fb8296260263815aff90ce43
ihorvatX <ivan.horvat1@vuv.hr>
2021-03-31 09:55:48 +0200

    dodan stupac checkbox koji mjenja boju kad se klikne na red

--- js/app.js
+++ js/app.js
@@ -13,11 +13,13 @@ var cell1 = row.insertCell(0);
 var cell2 = row.insertCell(1);
 var cell3 = row.insertCell(2);
 var cell4 = row.insertCell(3);
+var cell5 = row.insertCell(4)
 
 cell1.innerHTML = subject;
 cell2.innerHTML = year;
 cell3.innerHTML = ects;
-cell4.innerHTML = '<button class="DeleteIcon" onclick="DeleteRow(this)"></button>'
+cell4.innerHTML = '<button class="DeleteIcon" onclick="DeleteRow(this)"></button>';
+cell5.innerHTML = ' <input type="checkbox" id="myCheck" onclick="ChangeColor(this)">'
 
 }
 
@@ -25,4 +27,18 @@ function DeleteRow(o)
 {
     var p=o.parentNode.parentNode;
         p.parentNode.removeChild(p);
+}
+
+function ChangeColor(o)
+{
+    var p=o.parentNode.parentNode;
+    if(o.checked)
+    {
+        p.setAttribute('style', 'background: red');
+    }
+    else
+    {
+        p.setAttribute('style', 'background: white');
+    }
+
 }
