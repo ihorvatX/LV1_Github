7b894da237afb21732b5d3e31a416d5624114e75
ihorvatX <ivan.horvat1@vuv.hr>
2021-03-31 09:35:13 +0200

    Dodan stupac u tablici akcije koji brise redak na koji se klikne ta akcija

--- js/app.js
+++ js/app.js
@@ -5,24 +5,24 @@ function SaveToTable()
     let year = document.getElementById('year').value;
     let ects = document.getElementById('ects').value;
 
-    
-   // Find a <table> element with id="myTable":
-var table = document.getElementById("table");
+    var table = document.getElementById("table");
 
-// Create an empty <tr> element and add it to the 1st position of the table:
 var row = table.insertRow(1);
 
-// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
 var cell1 = row.insertCell(0);
 var cell2 = row.insertCell(1);
-var cell3 = row.insertCell(2)
+var cell3 = row.insertCell(2);
+var cell4 = row.insertCell(3);
 
-// Add some text to the new cells:
 cell1.innerHTML = subject;
 cell2.innerHTML = year;
 cell3.innerHTML = ects;
+cell4.innerHTML = '<button class="DeleteIcon" onclick="DeleteRow(this)"></button>'
 
+}
 
-
-
+function DeleteRow(o) 
+{
+    var p=o.parentNode.parentNode;
+        p.parentNode.removeChild(p);
 }
